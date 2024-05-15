78fa9824d0f8493648328e8b4739ac33a2855cbb
chryzcode <alabaolanrewaju13@gmail.com>
2024-05-15 10:22:02 +0100

    push

---
+++ app.js
@@ -0,0 +1,69 @@
+import "dotenv/config";
+import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+
+import session from "express-session";
+import passport from "passport";
+import "./utils/passport.js";
+
+//error handler
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
+//import route
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100, //limit each ip to 100 requests per windowsMs
+  })
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(
+    `Topspot Property Management API`
+  );
+});
+
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`server is running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ models/user.js
@@ -0,0 +1,91 @@
+import "dotenv/config";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Please provide full name"],
+    },
+    avatar: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+$/, "Please provide a valid username"],
+    },
+
+    email: {
+      required: true,
+      type: String,
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    recoveryEmail: {
+      type: String,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    Country: {
+      type: String,
+    },
+
+    State: {
+      type: String,
+    },
+
+    City: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 5,
+    },
+    token: {
+      type: String,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.createJWT = function () {
+  const token = jwt.sign({ userId: this._id, fullName: this.fullName }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  this.token = token;
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+const User = mongoose.model("User", userSchema);
+
+export { User };

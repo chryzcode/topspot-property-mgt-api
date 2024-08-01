bbabf6576cf4bbdce37b633f254c9905698a1a67
chryzcode <alabaolanrewaju13@gmail.com>
2024-08-01 13:33:58 +0100

    push

--- routes/payment.js
+++ routes/payment.js
@@ -5,8 +5,8 @@ import authenticateUser from "../middleware/authentication.js";
 
 const router = express.Router();
 
-router.route("/cancel-payment/:serviceId").get(authenticateUser, cancelPayment);
-router.route("/successful-payment/:serviceId").post(authenticateUser, successfulPayment);
+router.route("/cancel-payment/:serviceId").get(cancelPayment);
+router.route("/successful-payment/:serviceId").post(successfulPayment);
 router.route("/make-payment/:serviceId").post(authenticateUser, makePayment);
 
 export default router;

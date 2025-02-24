126e245c1b3dce74c5f4a205818b237714eff48a
Olanrewaju Alaba <alabaolanrewaju13@gmail.com>
2025-02-24 09:59:16 +0100

    chnages on successful payment validation

--- routes/payment.js
+++ routes/payment.js
@@ -5,8 +5,8 @@ import authenticateUser from "../middleware/authentication.js";
 
 const router = express.Router();
 
-router.route("/:serviceId/cancel-payment/:userId").get(cancelPayment);
-router.route("/:serviceId/successful-payment/:userId").post(successfulPayment);
+router.route("/:serviceId/cancel-payment").get(cancelPayment);
+router.route("/:serviceId/successful-payment").post(authenticateUser, successfulPayment);
 router.route("/make-payment/:serviceId").post(authenticateUser, makePayment);
 
 export default router;
